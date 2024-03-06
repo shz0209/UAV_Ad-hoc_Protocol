321627ee6dd45d96fc6ec017dc7ee2193a46a572
BinciLuo <binciluo@icloud.com>
2024-03-06 14:36:05 +0800

    [add] add utils_events.h

--- ci_test.sh
+++ ci_test.sh
@@ -3,8 +3,14 @@ echo "Test script: test.sh"
 bash test.sh
 
 echo '------------------------------------------------------------'
-echo "g++  -I/usr/include/libnl3 -lnl-route-3 -lnl-3 -std=c++11 src/*.cpp test_utils/test_utils_events.cpp -o utils_events"
+echo "Test script: g++  -I/usr/include/libnl3 -lnl-route-3 -lnl-3 -std=c++11 src/*.cpp test_utils/test_utils_events.cpp -o utils_events"
 g++  -I/usr/include/libnl3 -lnl-route-3 -lnl-3 -std=c++11 src/*.cpp test_utils/test_utils_events.cpp -o utils_events
+if [ $? -eq 0 ]; then
+    echo "Compilation successful"
+else
+    echo "Compilation failed"
+    exit 1
+fi
 
 echo '------------------------------------------------------------'
 echo "All test passed."
--- src/utils_events.cpp
+++ src/utils_events.cpp
@@ -98,3 +98,13 @@ void receive_netlink_messages(int sock_fd) {
         }
     }
 }
+
+int main() {
+    int sock_fd = create_netlink_socket();
+    bind_netlink_socket(sock_fd);
+    receive_netlink_messages(sock_fd);
+
+    // 不会执行到这里，因为循环监听会一直进行
+    close(sock_fd);
+    return 0;
+}
